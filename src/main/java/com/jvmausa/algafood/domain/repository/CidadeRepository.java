cf9dbf66ccd9a0c83f28a8fe465d662abcd019e3
Joao Victor do Canto <jvmausa@gmail.com>
2021-04-16 20:26:46 -0300

    addes Cidade
    entity
    repository
    implements

---
+++ src/main/java/com/jvmausa/algafood/domain/repository/CidadeRepository.java
@@ -0,0 +1,17 @@
+package com.jvmausa.algafood.domain.repository;
+
+import java.util.List;
+
+import com.jvmausa.algafood.domain.model.Cidade;
+
+public interface CidadeRepository {
+
+	List<Cidade> listar();
+
+	Cidade buscar(Long id);
+
+	Cidade salvar(Cidade cidade);
+
+	void remover(Cidade cidade);
+
+}
---
+++ src/main/java/com/jvmausa/algafood/infrastructure/repository/CidadeImpl.java
@@ -0,0 +1,46 @@
+package com.jvmausa.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Component;
+
+import com.jvmausa.algafood.domain.model.Cidade;
+import com.jvmausa.algafood.domain.repository.CidadeRepository;
+
+
+@Component
+public class CidadeImpl implements CidadeRepository{
+
+	@PersistenceContext
+	EntityManager manager;
+	
+	
+	@Override
+	public List<Cidade> listar() {
+		return manager.createQuery("from cidade", Cidade.class).getResultList();
+	}
+
+	@Override
+	public Cidade buscar(Long id) {
+		return manager.find(Cidade.class, id);
+	}
+	
+	@Transactional
+	@Override
+	public Cidade salvar(Cidade cidade) {
+		return manager.merge(cidade);
+	}
+
+	@Transactional
+	@Override
+	public void remover(Cidade cidade) {
+		cidade = buscar(cidade.getId());
+		manager.remove(cidade);
+		
+	}
+
+}
