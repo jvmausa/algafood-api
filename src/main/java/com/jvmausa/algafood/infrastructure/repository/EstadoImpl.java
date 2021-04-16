892a57d7dbe1585ec3462b51c8af80becaf6f04b
Joao Victor do Canto <jvmausa@gmail.com>
2021-04-16 20:26:21 -0300

    added Estado
    entity
    repository
    implements

---
+++ src/main/java/com/jvmausa/algafood/infrastructure/repository/EstadoImpl.java
@@ -0,0 +1,43 @@
+package com.jvmausa.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Component;
+
+import com.jvmausa.algafood.domain.model.Estado;
+import com.jvmausa.algafood.domain.repository.EstadoRepository;
+
+@Component
+public class EstadoImpl implements EstadoRepository {
+
+	@PersistenceContext
+	EntityManager manager;
+
+	@Override
+	public List<Estado> listar() {
+		return manager.createQuery("from estado", Estado.class).getResultList();
+	}
+
+	@Override
+	public Estado buscar(Long id) {
+		return manager.find(Estado.class, id);
+	}
+
+	@Transactional
+	@Override
+	public Estado salvar(Estado estado) {
+		return manager.merge(estado);
+	}
+
+	@Transactional
+	@Override
+	public void remover(Estado estado) {
+		estado = buscar(estado.getId());
+		manager.remove(estado);
+	}
+
+}
