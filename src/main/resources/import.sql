9cea467fd12a49199ce4c713b1a8faba297fef3d
Joao Victor do Canto <jvmausa@gmail.com>
2021-04-16 20:50:02 -0300

    Update import.sql
    added news inserts

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,16 @@ insert into cozinha (nome) values ('Japonesa');
 
 insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('Thai Food', 12.90, 1);
 insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('San Tao', 9.90, 2);
+
+insert into estado(nome) values ('Rio Grande do Sul');
+insert into estado(nome) values ('Rio de Janeiro');
+
+insert into cidade(nome, estado_id) values ('Canoas', 1);
+insert into cidade(nome, estado_id) values ('Rio de Janeiro', 2);
+
+
+insert into forma_pagamento(descricao) values ('Crédito');
+insert into forma_pagamento(descricao) values ('Débito');
+
+insert into permissao(nome, descricao) values ('admin', 'Todas as permissões');
+insert into permissao(nome, descricao) values ('usuario_comum', 'Permite realizar compra');
