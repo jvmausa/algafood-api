be7ed2210ab6206cfab0156ec9b7dd8261082d43
Joao Victor do Canto <jvmausa@gmail.com>
2021-04-17 16:41:11 -0300

    Update CozinhaRepositoryImpl.java
    added comment

--- src/main/java/com/jvmausa/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/jvmausa/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -19,7 +19,7 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 
 	public List<Cozinha> listar() {
 
-		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+		return manager.createQuery("from Cozinha", Cozinha.class).getResultList(); // from "<classe Java>", e não tabela SQL
 
 	}
 
