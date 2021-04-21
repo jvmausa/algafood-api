045a379afae6b6290847498a483d21a0b620931c
Joao Victor do Canto <jvmausa@gmail.com>
2021-04-21 18:13:47 -0300

    Update CozinhaController.java
    + tratamento de erro retornando 404 not found em Buscar
    + adicionar method retornando corpo

--- src/main/java/com/jvmausa/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/jvmausa/algafood/api/controller/CozinhaController.java
@@ -3,13 +3,15 @@ package com.jvmausa.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jvmausa.algafood.api.model.CozinhasXmlWrapper;
@@ -41,14 +43,29 @@ public class CozinhaController {
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
 
 		Cozinha cozinha = cozinhaRepository.buscar(id);
-		//return ResponseEntity.status(HttpStatus.OK).body(cozinha); << MAIS USADO QUANDO HÁ CONDICIONAIS
+		//return ResponseEntity.status(HttpStatus.OK).body(cozinha); << MAIS USADO QUANDO HÁ CONDICIONAIS QUE RETORNAM HttpStatus
 		//return ResponseEntity.ok(cozinha);
 		
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+		//HttpHeaders headers = new HttpHeaders();
+		//headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");	
+		//return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
 		
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		} 
 		
-		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+		return ResponseEntity.notFound().build();
+		
 	}
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		
+		return cozinhaRepository.salvar(cozinha);
+		
+	}
+	
+	
+	
 
 }
