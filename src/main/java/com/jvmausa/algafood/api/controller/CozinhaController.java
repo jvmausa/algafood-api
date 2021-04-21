b99a9d988f321ea1dae28c793026bb25fbbc70b9
Joao Victor do Canto <jvmausa@gmail.com>
2021-04-21 17:45:08 -0300

    Create CozinhaController.java
    controller Cozinha

---
+++ src/main/java/com/jvmausa/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,54 @@
+package com.jvmausa.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jvmausa.algafood.api.model.CozinhasXmlWrapper;
+import com.jvmausa.algafood.domain.model.Cozinha;
+import com.jvmausa.algafood.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Cozinha> listarJson() {
+
+		return cozinhaRepository.listar();
+
+	}
+
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml() {
+
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}
+
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+
+		Cozinha cozinha = cozinhaRepository.buscar(id);
+		//return ResponseEntity.status(HttpStatus.OK).body(cozinha); << MAIS USADO QUANDO HÁ CONDICIONAIS
+		//return ResponseEntity.ok(cozinha);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+		
+		
+		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+	}
+
+}
