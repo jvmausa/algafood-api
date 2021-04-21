b656ec60d1589659811fddf57b0ba8e01328e438
Joao Victor do Canto <jvmausa@gmail.com>
2021-04-21 17:44:43 -0300

    Create CozinhasXmlWrapper.java
    class to model XML

---
+++ src/main/java/com/jvmausa/algafood/api/model/CozinhasXmlWrapper.java
@@ -0,0 +1,26 @@
+package com.jvmausa.algafood.api.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.jvmausa.algafood.domain.model.Cozinha;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@JacksonXmlRootElement(localName = "cozinhas")
+@Data
+public class CozinhasXmlWrapper {
+	
+	@JsonProperty("cozinha")
+	@JacksonXmlElementWrapper(useWrapping = false)
+	@NonNull //gera construtor com propriedades obrigatórias
+	private List<Cozinha> cozinhas;
+	
+	
+	
+	
+
+}
