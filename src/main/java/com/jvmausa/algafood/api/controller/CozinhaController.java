da0d88e1daf86ca0176eeee5ae9686a13faa0181
Joao Victor do Canto <jvmausa@gmail.com>
2021-04-21 18:36:04 -0300

    Update CozinhaController.java
    added update method, atualizar com tratamento de null
    usando BeanUtils pra copiar propriedades

--- src/main/java/com/jvmausa/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/jvmausa/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.jvmausa.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,34 +40,48 @@ public class CozinhaController {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 
-
 	@GetMapping("/{id}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
 
 		Cozinha cozinha = cozinhaRepository.buscar(id);
-		//return ResponseEntity.status(HttpStatus.OK).body(cozinha); << MAIS USADO QUANDO HÁ CONDICIONAIS QUE RETORNAM HttpStatus
-		//return ResponseEntity.ok(cozinha);
-		
-		//HttpHeaders headers = new HttpHeaders();
-		//headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");	
-		//return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
-		
+		// return ResponseEntity.status(HttpStatus.OK).body(cozinha); << MAIS USADO
+		// QUANDO HÁ CONDICIONAIS QUE RETORNAM HttpStatus
+		// return ResponseEntity.ok(cozinha);
+
+		// HttpHeaders headers = new HttpHeaders();
+		// headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+		// return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+
 		if (cozinha != null) {
 			return ResponseEntity.ok(cozinha);
-		} 
-		
+		}
+
 		return ResponseEntity.notFound().build();
-		
+
 	}
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
-		
+
 		return cozinhaRepository.salvar(cozinha);
-		
+
 	}
-	
-	
-	
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha) {
+
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(id);
+
+		if (cozinhaAtual != null) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id"); // entre "" é o parâmetro que deve ser ignorado na
+																	// cópia
+			cozinhaRepository.salvar(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
+
+	}
 
 }
