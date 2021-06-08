b80156b7319abe541e27078cf8f65fed226bd832
Joao Victor do Canto <jvmausa@gmail.com>
2021-06-08 11:34:24 -0300

    Update Cozinha.java
    adicionada anotação para correção de erro de looping

--- src/main/java/com/jvmausa/algafood/domain/model/Cozinha.java
+++ src/main/java/com/jvmausa/algafood/domain/model/Cozinha.java
@@ -1,11 +1,16 @@
 package com.jvmausa.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
@@ -23,11 +28,15 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	
 	//@Column(name = "nom_cozinha")
 	//@JsonIgnore remove da representação este atributo
 	//@JsonProperty("titulo") altera o nome deste atributo na representação
 	@Column(nullable = false)
 	private String nome;
-
+	
+	// @JsonIgnore para ignorar na representação, caso contrário fica em looping(neste caso)
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>(); 
+	//cozinha tem muitos restaurantes. "Many" sempre significa que a propriedade é uma coleção
 }
