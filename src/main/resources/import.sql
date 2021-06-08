2f6a91ba5d66fae8ea66c052160e24bee9166a4b
Joao Victor do Canto <jvmausa@gmail.com>
2021-06-08 11:29:45 -0300

    Update import.sql
    adicionadas valores para tabela forma pagamento

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,9 +10,10 @@ insert into estado(nome) values ('Rio de Janeiro');
 insert into cidade(nome, estado_id) values ('Canoas', 1);
 insert into cidade(nome, estado_id) values ('Rio de Janeiro', 2);
 
-
 insert into forma_pagamento(descricao) values ('Crédito');
 insert into forma_pagamento(descricao) values ('Débito');
 
 insert into permissao(nome, descricao) values ('admin', 'Todas as permissões');
 insert into permissao(nome, descricao) values ('usuario_comum', 'Permite realizar compra');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values(1, 1), (1, 2), (2,1), (2,2);
