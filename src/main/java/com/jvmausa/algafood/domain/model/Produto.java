1a35009987a3544d8a387d7b6e10ace263ffb30d
Joao Victor do Canto <jvmausa@gmail.com>
2021-06-21 19:56:37 -0300

    Update Produto.java

--- src/main/java/com/jvmausa/algafood/domain/model/Produto.java
+++ src/main/java/com/jvmausa/algafood/domain/model/Produto.java
@@ -2,9 +2,12 @@ package com.jvmausa.algafood.domain.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Produto {
 	
 	private BigDecimal id;
