a0d95921b191cd68c695a8eae9596ddb8d33a32a
Joao Victor do Canto <jvmausa@gmail.com>
2021-07-20 18:15:00 -0300

    criado novo handler para URL errada

--- src/main/java/com/jvmausa/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/jvmausa/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.flywaydb.core.internal.util.ExceptionUtils;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.fasterxml.jackson.databind.JsonMappingException.Reference;
@@ -30,9 +32,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		Throwable rootCause = ExceptionUtils.getRootCause(ex);
 		
 		if(rootCause instanceof InvalidFormatException) {
-				return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
+				return handleInvalidFormat((InvalidFormatException) rootCause, headers, status, request);
 		}else if(rootCause instanceof PropertyBindingException) {
-	        return handlePropertyBindingException((PropertyBindingException) rootCause, headers, status, request); 
+	        return handlePropertyBinding((PropertyBindingException) rootCause, headers, status, request); 
 	    }
 		
 		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
@@ -44,7 +46,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 	
 	
-	private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex,
+	private ResponseEntity<Object> handleInvalidFormat(InvalidFormatException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
 		String path = joinPath(ex.getPath());
@@ -59,8 +61,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
 	
-	@ExceptionHandler
-	private ResponseEntity<Object> handlePropertyBindingException(PropertyBindingException ex,
+
+	private ResponseEntity<Object> handlePropertyBinding(PropertyBindingException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request){
 		
 		String path = joinPath(ex.getPath());
@@ -74,9 +76,34 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
 	
+	@Override
+	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+		if (ex instanceof MethodArgumentTypeMismatchException) {
+			return handleMethodArgumentTypeMismatch(
+					(MethodArgumentTypeMismatchException) ex, headers, status, request);
+		}
+		
+		return super.handleTypeMismatch(ex, headers, status, request);
+	}
+	
+	private ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request){
+		 
+		ProblemType problemType = ProblemType.PARAMENTO_INVALIDO;
+		
+		String detail = String.format("O parâmetro de URL '%s' recebeu o valor '%s' " 
+		+ "que é do tipo inválido. Corrija e informe um valor compatível com o tipo '%s'",
+		ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problem, headers, status, request);
+	}
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex,
+	public ResponseEntity<?> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException ex,
 			WebRequest request) {
 
 		HttpStatus status = HttpStatus.NOT_FOUND;
@@ -92,7 +119,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
+	public ResponseEntity<?> handleEntidadeEmUso(EntidadeEmUsoException ex, WebRequest request) {
 
 		HttpStatus status = HttpStatus.CONFLICT;
 
--- src/main/java/com/jvmausa/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/jvmausa/algafood/api/exceptionhandler/ProblemType.java
@@ -8,7 +8,8 @@ public enum ProblemType {
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensivel"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
-	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");  
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+	PARAMENTO_INVALIDO("/parametro-invalido", "Parametro Inválido");
 			
 	private String title;
 	private String uri;
