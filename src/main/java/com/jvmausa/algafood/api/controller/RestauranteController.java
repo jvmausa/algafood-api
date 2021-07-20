7b50bd86ef6736c917a6608a76796f405c7e6ffe
Joao Victor do Canto <jvmausa@gmail.com>
2021-07-20 17:44:43 -0300

    refatorando metodo para entrar nas handles criadas anteriormente
    agora solicitações PATCH com erros caem nas handles criadas

--- src/main/java/com/jvmausa/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/jvmausa/algafood/api/controller/RestauranteController.java
@@ -4,9 +4,14 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jvmausa.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.jvmausa.algafood.domain.exception.NegocioException;
@@ -79,37 +85,47 @@ public class RestauranteController {
 	}
 
 	@PatchMapping("/{id}")
-	public Restaurante atualizarParcial(@PathVariable Long id, @RequestBody Map<String, Object> campos) {
+	public Restaurante atualizarParcial(@PathVariable Long id, @RequestBody Map<String, Object> campos, HttpServletRequest request) {
 		Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(id);
 
-		merge(campos, restauranteAtual);
+		merge(campos, restauranteAtual, request);
 
 		return atualizar(id, restauranteAtual);
 
 	}
 
-	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request ) {
+		
+		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
+		
+		
+		try {
+			/*
+			 * >objectMapper.convertValue() converte o tipo de valor que foi inserido no
+			 * JSON pro valor do atribudo correspondente da classe Restaurante ex: no JSON é
+			 * recebido como INTEIRO e é preciso que seja BIDECIMAL.
+			 */
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
 
-		/*
-		 * >objectMapper.convertValue() converte o tipo de valor que foi inserido no
-		 * JSON pro valor do atribudo correspondente da classe Restaurante ex: no JSON é
-		 * recebido como INTEIRO e é preciso que seja BIDECIMAL.
-		 */
-		ObjectMapper objectMapper = new ObjectMapper();
-		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+				field.setAccessible(true);
 
-		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-			field.setAccessible(true);
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
 
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+				// System.out.println(nomePropriedade + " = " + valorPropriedade + " = " +
+				// novoValor);
 
-			// System.out.println(nomePropriedade + " = " + valorPropriedade + " = " +
-			// novoValor);
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
 
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
-
-		});
+			});
+		} catch (IllegalArgumentException e) {
+			Throwable rootCause = ExceptionUtils.getRootCause(e); //classe do apache commonslang3 para pegar a causa da exception
+			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+		}
+		
 	}
 
 	/*
