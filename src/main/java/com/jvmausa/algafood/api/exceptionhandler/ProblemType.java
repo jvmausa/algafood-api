eae3232d4d616f34ef979a2b9782fae467182dae
Joao Victor do Canto <jvmausa@gmail.com>
2021-07-21 17:08:22 -0300

    adicionado handleException para dados inválidos

--- src/main/java/com/jvmausa/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/jvmausa/algafood/api/exceptionhandler/ProblemType.java
@@ -10,7 +10,8 @@ public enum ProblemType {
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
 	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de Sistema"),
-	PARAMENTO_INVALIDO("/parametro-invalido", "Parametro Inválido");
+	PARAMENTO_INVALIDO("/parametro-invalido", "Parametro Inválido"),
+	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos");
 			
 	
 	private String title;
