71f2310ebb91fb9e946eee50eaeb7b03e45d2582
Joao Victor do Canto <jvmausa@gmail.com>
2021-07-28 18:52:28 -0300

    refatoração para adicionar mensagens de validação a nivel de classe

--- src/main/java/com/jvmausa/algafood/domain/model/Restaurante.java
+++ src/main/java/com/jvmausa/algafood/domain/model/Restaurante.java
@@ -19,7 +19,6 @@ import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
@@ -28,6 +27,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jvmausa.algafood.core.validation.Groups;
+import com.jvmausa.algafood.core.validation.TaxaFrete;
 import com.jvmausa.algafood.core.validation.ValorZeroIncluiDescricao;
 
 import lombok.Data;
@@ -53,10 +53,9 @@ public class Restaurante {
 
 	//@PositiveOrZero (message = "{TaxaFrete.Invalida}")
 	//@DecimalMin("0")
-	//@TaxaFrete
 	//@Multiplo(numero = 5)
+	@TaxaFrete
 	@NotNull
-	@PositiveOrZero
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
