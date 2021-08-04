ef9f981bf5c06d10068c5dc173e2f693f13f9882
Joao Victor do Canto <jvmausa@gmail.com>
2021-08-04 18:48:29 -0300

    created a "prepare data before API tests" method making it more professional

--- src/test/java/com/jvmausa/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/jvmausa/algafood/CadastroCozinhaIT.java
@@ -7,31 +7,44 @@ import static org.hamcrest.Matchers.hasSize;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.jvmausa.algafood.domain.model.Cozinha;
+import com.jvmausa.algafood.domain.repository.CozinhaRepository;
+import com.jvmausa.algafood.util.DatabaseCleaner;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
 	
 	@LocalServerPort
 	private int port;
-
+	
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	//método que será executado antes de cada teste
 	@BeforeEach
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
-
-	}
-	
-	
-	
+		
+		databaseCleaner.clearTables();
+		prepararDados();
+	}	
 	
 	@Test
 	public void deveRetornar200_ConsultarCozinhar() {
@@ -46,14 +59,14 @@ class CadastroCozinhaIT {
 
 
 	@Test
-	public void deveRetornar4Cozinhas_ConsultarCozinhar() {
+	public void deveRetornar2Cozinhas_ConsultarCozinhar() {
 		given()
 		.accept(ContentType.JSON)
 	.when()
 		.get()
 	.then()
-		.body("", hasSize(4))
-		.body("nome", hasItems("Indiana", "Tailandesa"));
+		.body("", hasSize(2))
+		.body("nome", hasItems("Americana", "Tailandesa"));
 		
 	}
 	
@@ -69,5 +82,19 @@ class CadastroCozinhaIT {
 		.statusCode(HttpStatus.CREATED.value());
 		
 	}
+	
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Tailandesa");
+		cozinhaRepository.save(cozinha1);
+		
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Americana");
+		cozinhaRepository.save(cozinha2);
+		
+		
+		
+	}
+	
 	
 }
