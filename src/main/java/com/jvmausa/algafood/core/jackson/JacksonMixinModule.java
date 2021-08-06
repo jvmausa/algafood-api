0ad8c3088fd99046b8c363f31fd2c257d9b0c016
Joao Victor do Canto <jvmausa@gmail.com>
2021-08-06 20:08:38 -0300

    deletd Mixin Restaurante class. Useless

--- src/main/java/com/jvmausa/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/jvmausa/algafood/core/jackson/JacksonMixinModule.java
@@ -5,10 +5,8 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.jvmausa.algafood.api.model.mixin.CidadeMixin;
 import com.jvmausa.algafood.api.model.mixin.CozinhaMixin;
-import com.jvmausa.algafood.api.model.mixin.RestauranteMixin;
 import com.jvmausa.algafood.domain.model.Cidade;
 import com.jvmausa.algafood.domain.model.Cozinha;
-import com.jvmausa.algafood.domain.model.Restaurante;
 
 
 @Component
@@ -17,7 +15,6 @@ public class JacksonMixinModule extends SimpleModule {
 	private static final long serialVersionUID = 1L;
 
 	public JacksonMixinModule() {
-		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
 		setMixInAnnotation(Cidade.class, CidadeMixin.class);
 		setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
 	}
