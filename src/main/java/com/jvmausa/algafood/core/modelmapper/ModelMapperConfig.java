b9272d8c1400faab1a4f8bc6ffcd7cdb74a44478
Joao Victor do Canto <jvmausa@gmail.com>
2021-08-07 17:47:25 -0300

    didactic case for custom attribute mapping
    ModelMapper can recognize a attribute even without any name match.
    precoFrete /= taxaFrete e.g, so a custom mapping for those got to be done

--- src/main/java/com/jvmausa/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/jvmausa/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,12 +4,27 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.jvmausa.algafood.api.model.RestauranteModel;
+import com.jvmausa.algafood.domain.model.Restaurante;
+
 @Configuration
 public class ModelMapperConfig {
 
+	/*
+	 * exemplo de customização de mapeamento de propriedade
+	 */
+	
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+		
+		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+		.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+		
+		return modelMapper;
 	}
+	
+	
+	
 	
 }
