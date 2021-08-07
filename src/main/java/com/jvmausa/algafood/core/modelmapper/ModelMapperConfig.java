020312fdac9b2d71258692b7299f86c0be754192
Joao Victor do Canto <jvmausa@gmail.com>
2021-08-07 17:49:51 -0300

    last commit undone

--- src/main/java/com/jvmausa/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/jvmausa/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,27 +4,17 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.jvmausa.algafood.api.model.RestauranteModel;
-import com.jvmausa.algafood.domain.model.Restaurante;
-
 @Configuration
 public class ModelMapperConfig {
 
 	/*
 	 * exemplo de customização de mapeamento de propriedade
 	 */
-	
+
 	@Bean
 	public ModelMapper modelMapper() {
-		var modelMapper = new ModelMapper();
-		
-		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
-		.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
-		
-		return modelMapper;
+		return new ModelMapper();
+
 	}
-	
-	
-	
-	
+
 }
