63ba857ad6085361722c94222b8d3074e1b47093
Joao Victor do Canto <jvmausa@gmail.com>
2021-08-17 11:27:21 -0300

    handling BindException for URL mismatch params

--- src/main/java/com/jvmausa/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/jvmausa/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -135,55 +135,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
-	
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		
-		return extractedHandleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
-	}
 
-	//method extraído para reutilização
-	private ResponseEntity<Object> extractedHandleValidationInternal(Exception ex, BindingResult bindingResult,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		
-		ProblemType problemType = ProblemType.DADOS_INVALIDOS;
-				
-		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
-		
-		 List<Problem.Object> problemObjects = bindingResult.getAllErrors().stream()
-		    		.map(objectError -> {
-		    				String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
-		    				
-		    				String name = objectError.getObjectName();
-		    				
-		    				if(objectError instanceof FieldError) {
-		    					
-		    					name = ((FieldError) objectError).getField();
-		    				}
-		    				
-		    				return Problem.Object.builder()
-		    				.name(name)
-		    				.userMessage(message)
-		    				.build();
-		    		})
-		    		.collect(Collectors.toList());
-		
-		Problem problem = createProblemBuilder(status, problemType, detail)
-				.userMessage(detail)
-				.objects(problemObjects)
-				.build();
-		
-		return handleExceptionInternal(ex, problem, headers, status, request);
-	}
 	
 
 	@ExceptionHandler({ValidacaoException.class})
 	public ResponseEntity<?> handleValidacaoException(ValidacaoException ex, WebRequest request) {
 		
-	    return extractedHandleValidationInternal(ex, ex.getBindingResult(), new HttpHeaders(), 
+	    return handleValidationInternal(ex, ex.getBindingResult(), new HttpHeaders(), 
 	            HttpStatus.BAD_REQUEST, request);
-		
 		
 	}
 	
@@ -273,7 +232,56 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
+	
+	@Override
+	protected ResponseEntity<Object> handleBindException(org.springframework.validation.BindException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
+	}
 
+
+	private ResponseEntity<Object> handleValidationInternal(Exception ex, BindingResult bindingResult,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		ProblemType problemType = ProblemType.DADOS_INVALIDOS;
+				
+		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
+		
+		 List<Problem.Object> problemObjects = bindingResult.getAllErrors().stream()
+		    		.map(objectError -> {
+		    				String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
+		    				
+		    				String name = objectError.getObjectName();
+		    				
+		    				if(objectError instanceof FieldError) {
+		    					
+		    					name = ((FieldError) objectError).getField();
+		    				}
+		    				
+		    				return Problem.Object.builder()
+		    				.name(name)
+		    				.userMessage(message)
+		    				.build();
+		    		})
+		    		.collect(Collectors.toList());
+		
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(detail)
+				.objects(problemObjects)
+				.build();
+		
+		return handleExceptionInternal(ex, problem, headers, status, request);
+	}
+	
+	
 	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
 
 		return Problem.builder()
