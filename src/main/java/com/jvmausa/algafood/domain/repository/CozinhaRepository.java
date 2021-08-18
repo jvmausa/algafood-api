c237e558c8d701601f8f986c01315ad26ae96dca
Joao Victor do Canto <jvmausa@gmail.com>
2021-08-18 13:26:56 -0300

    adding pageable content + an example for repository

--- src/main/java/com/jvmausa/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/jvmausa/algafood/domain/repository/CozinhaRepository.java
@@ -2,6 +2,8 @@ package com.jvmausa.algafood.domain.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import com.jvmausa.algafood.domain.model.Cozinha;
@@ -9,6 +11,8 @@ import com.jvmausa.algafood.domain.model.Cozinha;
 @Repository
 public interface CozinhaRepository extends CustomJpaRepository<Cozinha, Long> {
 
+	Page<Cozinha> findTodosByNome(String nome, Pageable pageable); // exemplo com PAGINAÇÃO
+	
 	List<Cozinha> findTodosByNome(String nome); // pesquisa todas por nome exato
 	
 	List<Cozinha> findTodosByNomeContaining(String nome); // por nome contendo
