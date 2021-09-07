2e0ab06c64690c47afd302f8515a1a997e6c1ddd
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2021-09-07 16:50:16 -0300

    describing http status code for GET requests globally

--- src/main/java/com/jvmausa/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/jvmausa/algafood/core/openapi/SpringFoxConfig.java
@@ -1,14 +1,22 @@
 package com.jvmausa.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -16,6 +24,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig implements WebMvcConfigurer{
 
 	@Bean
@@ -25,16 +34,30 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 					.apis(RequestHandlerSelectors.basePackage("com.jvmausa.algafood.api"))
 //					.paths(PathSelectors.ant("/restaurantes/*"))
 					.build()
+				.useDefaultResponseMessages(false)
+				.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia Cidades"));
 	}
 	
-	public ApiInfo apiInfo() {
+	private List<ResponseMessage> globalGetResponseMessages() {
+		return Arrays.asList(
+				new ResponseMessageBuilder()
+				.code(500)
+				.message("Erro Interno do servidor.")
+				.build(),
+				new ResponseMessageBuilder()
+				.code(406)
+				.message("Recurso não possui representado que poderia ser aceita pelo consumidor.")
+				.build()
+				);
+	}	
+	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 				.title("Alga Food API")
 				.description("API aberta para cliente")
 				.version("1.0.0")
-				.contact(new Contact("Joao", "http://google.com.br", "jvmausa@gmail.com"))
+				.contact(new Contact("Joao", null, "jvmausa@gmail.com"))
 				.build();
 	}	
 	
