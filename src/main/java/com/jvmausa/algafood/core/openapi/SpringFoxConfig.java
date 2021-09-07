c94940a6f5ddbe1fc573eb71eaebab58d5fa898d
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2021-09-07 12:30:19 -0300

    customizing API Documentation information

--- src/main/java/com/jvmausa/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/jvmausa/algafood/core/openapi/SpringFoxConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,8 +23,19 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("com.jvmausa.algafood.api"))
 //					.paths(PathSelectors.ant("/restaurantes/*"))
-					.build();
+					.build()
+				.apiInfo(apiInfo());
 	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Alga Food API")
+				.description("API aberta para cliente")
+				.version("1.0.0")
+				.contact(new Contact("Joao", "http://google.com.br", "jvmausa@gmail.com"))
+				.build();
+	}	
+	
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
