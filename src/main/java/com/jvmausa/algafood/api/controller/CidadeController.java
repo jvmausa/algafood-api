5ae2456af433d390e509bcf6bf961c018c2a5f16
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2021-09-17 09:54:46 -0300

    added New Utility class: ResourceUriHelper to add Uri in headers once a new POST is created

--- src/main/java/com/jvmausa/algafood/api/controller/CidadeController.java
+++ src/main/java/com/jvmausa/algafood/api/controller/CidadeController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jvmausa.algafood.api.ResourceUriHelper;
 import com.jvmausa.algafood.api.assembler.CidadeInputDisassembler;
 import com.jvmausa.algafood.api.assembler.CidadeModelAssembler;
 import com.jvmausa.algafood.api.model.CidadeModel;
@@ -63,10 +64,15 @@ public class CidadeController implements CidadeControllerOpenApi {
 	@PostMapping
 	public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
 		Cidade cidade = cidadeInputDisassembler.toDomainObject(cidadeInput);
-
+		
+		 cidade = cadastroCidade.salvar(cidade);
+		
 		try {
-			return cidadeModelAssembler.toModel(cadastroCidade.salvar(cidade));
-
+			CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
+			
+			ResourceUriHelper.addUriInResponseHeader(cidadeModel.getId());
+			
+			return cidadeModel;
 		} catch (EntidadeNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage(), e); // exception para http 409 bad request
 		}
