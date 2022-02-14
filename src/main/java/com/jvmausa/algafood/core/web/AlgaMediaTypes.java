496ef69f90fd701c3362aca52b431d4060a5907c
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2022-02-14 13:09:44 -0300

    Adicionada classe para auxiliar no HATEOAS quando muda o mediatype da classe Controller

---
+++ src/main/java/com/jvmausa/algafood/core/web/AlgaMediaTypes.java
@@ -0,0 +1,12 @@
+package com.jvmausa.algafood.core.web;
+
+import org.springframework.http.MediaType;
+
+public class AlgaMediaTypes {
+
+	
+	public static final String V1_APPLICATIN_JSON_VALUE = "application/vnd.algofood.v1+json";
+	
+	public static final MediaType V1_APPLICATIN_JSON = MediaType.valueOf(V1_APPLICATIN_JSON_VALUE);
+	
+}
