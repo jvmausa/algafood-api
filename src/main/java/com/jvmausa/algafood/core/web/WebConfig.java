9d54f16f3d3426ae1053a16f6ce9eecb93faa3d2
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2022-02-16 10:28:38 -0300

    versionamento + definiçãod e versão caso nada seja especificado

--- src/main/java/com/jvmausa/algafood/core/web/WebConfig.java
+++ src/main/java/com/jvmausa/algafood/core/web/WebConfig.java
@@ -5,6 +5,7 @@ import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,4 +22,12 @@ public class WebConfig implements WebMvcConfigurer {
 	public Filter shallowEtagHeaderFilter() {
 		return new ShallowEtagHeaderFilter();
 	}
+	
+	//definição do MediaType padrão caso nada seja especificado no Headers
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.defaultContentType(AlgaMediaTypes.V2_APPLICATIN_JSON);
+		
+	}
+	
 }
