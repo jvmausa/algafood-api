8500b72d7469e44d7f1af6d54c9c3cb281e86b65
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2022-03-09 08:46:20 -0300

    refactoring to insert BCrypt password in the DB

--- src/main/resources/db/test-data/afterMigrate.sql
+++ src/main/resources/db/test-data/afterMigrate.sql
@@ -82,7 +82,7 @@ insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Esp
 insert into grupo (nome) values ('Gerente'), ('Vendedor'), ('Secretária'), ('Cadastrador');
 
 insert into usuario (id, nome, email, senha, data_cadastro) values
-(1, 'João da Silva', 'joao.ger@algafood.com', '123', utc_timestamp),
+(1, 'João da Silva', 'joao.ger@algafood.com', '$2a$12$mUh6cGqoXq4mwewnmH8R3.qUEAtCB0Lvp58wUW4ifZC5E3yMTsW0a', utc_timestamp),
 (2, 'Maria Joaquina', 'maria.vnd@algafood.com', '123', utc_timestamp),
 (3, 'José Souza', 'jose.aux@algafood.com', '123', utc_timestamp),
 (4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '123', utc_timestamp),
