42fa40d634765fe0e10cba2ce6a68b41671b9755
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2022-03-11 11:13:27 -0300

    added PreAuthorized annotation to restrict access

--- src/main/java/com/jvmausa/algafood/api/v1/controller/CozinhaController.java
+++ src/main/java/com/jvmausa/algafood/api/v1/controller/CozinhaController.java
@@ -9,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,6 +51,8 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 	@Autowired
 	private PagedResourcesAssembler<Cozinha> pagedResourceAssembler;
 
+	@PreAuthorize("isAuthenticated()")
+	@Override
 	@GetMapping
 	public PagedModel<CozinhaModel> listar(@PageableDefault(size = 2) Pageable pageable) {
 		log.info("Consultando Cozinhas com páginas de {} registros", pageable.getPageSize());
@@ -73,6 +76,9 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 		return cozinhaModelAssembler.toModel(cozinha);
 	}
 
+	
+	@PreAuthorize("hasAuthority('EDITAR_COZINHAS')")
+	@Override
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public CozinhaModel adicionar(@RequestBody @Valid CozinhaInput cozinhaInput) {
